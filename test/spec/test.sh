de06e38145cf67f992bd658b136127210e8a4f80
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2017-09-17 15:24:36 -0700

    chore(tooling): do not fail on bad wait

--- test/spec/test.sh
+++ test/spec/test.sh
@@ -1,54 +1,56 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
+
+cd "$(dirname "$0")"
+START_DIR=$(pwd)
+
+function onExit() {
+  set +x
+  set +e
+  [ -n "${PID}" ] && kill "${PID}"
+}
+
+trap onExit EXIT
 
 function runTest() {
-  npm start &
-  PID=$!
-  sleep 5
-  curl -X DELETE http://localhost:3000/state
+  set -e
+  startServer
 
   terraform init
   terraform plan
   terraform apply
 
-  kill $PID
-  wait $PID
-  unset PID
+  stopServer
 }
 
-#
-# Returns 0 if terraform fails
-#
-function runNegativeTest() {
-  npm start &
+PID=
+function startServer() {
+  echo "Starting test server"
+  set -e
+  # Reminder: can't use `npm start`` becauase it returns the wrong pid
+  node -r babel-register ../../.. &
   PID=$!
+  echo "Started test server with PID ${PID}"
   sleep 5
   curl -X DELETE http://localhost:3000/state
-
-  set +e
-  terraform init
-  local EXIT_CODE=$?
-  set -e
-
-  set +e
-  kill $PID
-  wait $PID
-  unset PID
-  set -e
-
-  if [ "${EXIT_CODE}" == "0" ]; then
-    return 1
-  else
-    return 0
-  fi
 }
 
-trap 'set +e; [ -n "${PID}" ] && kill ${PID}' EXIT
-export DATABASE_URL=postgres://postgres:password@localhost:5432/postgres
+function stopServer() {
+  echo "Stopping test server with PID ${PID}"
+  set -e
+  kill $PID
+  set +e
+  wait $PID
+  set -e
+  unset PID
 
-cd "$(dirname "$0")"
-START_DIR=$(pwd)
+  echo "Stopped test server with PID ${PID}"
+
+}
+
+export DATABASE_URL=postgres://postgres:password@localhost:5432/postgres
 
 npm run clean
 
@@ -81,15 +83,23 @@ unset BASIC_AUTH_USER
 unset BASIC_AUTH_PASSWORD
 export BASIC_AUTH_USER=test
 export BASIC_AUTH_PASSWORD=test
+startServer
+
 cd "${START_DIR}"
 cd ./without-locking
-runNegativeTest
-EXIT_CODE=$?
 
-if [ "${EXIT_CODE}" != "0" ]; then
+set +e
+terraform init
+EXIT_CODE=$?
+set -e
+# We expect init to fail
+if [ "${EXIT_CODE}" == "0" ]; then
   echo "Auth test failed to require credentials"
   exit 1
 fi
+
+stopServer
+
 cd "${START_DIR}"
 cd ./without-locking-auth
 runTest
